ee11ba9ab58595e4bc685526d9364b170712b090
Juan Duarte <142144090+JuanDG7@users.noreply.github.com>
2025-08-28 18:17:51 -0300

    Primer commit - Inicializacion del proyecto

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+export default {
+  theme: {
+    extend: {
+      screens: {
+        "988px": "988px",
+        "1356px": "1356px",
+        "668px": "668px",
+      },
+      colors: {
+        brand: {
+          light2222: "#0fa9af",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
