82797bf86a6bc3474a7039f2858889f9736a1066
Juan Duarte <142144090+JuanDG7@users.noreply.github.com>
2025-09-25 02:23:56 -0300

    chore: agregar carpeta backend con sus archivos

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const feedRoutes = require("./routes/feed");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  next();
+});
+
+app.use("/feed", feedRoutes);
+app.listen(8080);
